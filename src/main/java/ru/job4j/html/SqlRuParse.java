eebdb1d73cfde77caf1bb00e8b6a369c2c175e36
oryngalikarimzhan <oryngalikarimzhan@github.com>
2021-12-12 15:04:29 +0600

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -6,14 +6,28 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.getElementsByTag("tbody").get(2).children();
-        List<Element> rowList = row.stream().skip(1).collect(Collectors.toList());
+        String url = "https://www.sql.ru/forum/job-offers";
+        List<Element> rowList = new ArrayList<>();
+        Document doc;
+        Elements row;
+        int linesToSkip = 1;
+        for (int i = 1; i != 6; i++) {
+            if (i > 1) {
+                linesToSkip = 4;
+            }
+            url = url + "/" + i;
+            doc = Jsoup.connect(url).get();
+            row = doc.getElementsByTag("tbody").get(2).children();
+            row.stream().skip(linesToSkip).forEach(rowList::add);
+            url = url.substring(0, url.length() - 2);
+        }
+
         for (Element tr : rowList) {
             Elements post = tr.select(".postslisttopic");
             for (Element td : post) {
@@ -23,5 +37,6 @@ public class SqlRuParse {
             }
             System.out.println(new SqlRuDateTimeParser().parse(tr.child(5).text()));
         }
+
     }
 }
