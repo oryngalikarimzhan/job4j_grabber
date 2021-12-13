ae7e4e368c7b388d94b83d162868aeb712c4a072
oryngalikarimzhan <oryngalikarimzhan@github.com>
2021-12-13 17:22:45 +0600

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210 #239028]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,39 +4,27 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import ru.job4j.grabber.utils.SqlRuDateTimeParser;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        String url = "https://www.sql.ru/forum/job-offers";
+        String url = "https://www.sql.ru/forum/job-offers/";
         List<Element> rowList = new ArrayList<>();
         Document doc;
         Elements row;
-        int linesToSkip = 1;
         for (int i = 1; i != 6; i++) {
-            if (i > 1) {
-                linesToSkip = 4;
-            }
-            url = url + "/" + i;
+            url = url + i;
             doc = Jsoup.connect(url).get();
-            row = doc.getElementsByTag("tbody").get(2).children();
-            row.stream().skip(linesToSkip).forEach(rowList::add);
-            url = url.substring(0, url.length() - 2);
+            row = doc.select(".postslisttopic");
+            row.stream().skip(4).forEach(rowList::add);
+            url = url.substring(0, url.length() - 1);
         }
-
-        for (Element tr : rowList) {
-            Elements post = tr.select(".postslisttopic");
-            for (Element td : post) {
-                Element href = td.child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-            }
-            System.out.println(new SqlRuDateTimeParser().parse(tr.child(5).text()));
+        for (Element td : rowList) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
         }
-
     }
+
 }
