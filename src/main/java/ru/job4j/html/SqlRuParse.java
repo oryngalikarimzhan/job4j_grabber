933b0c159f6e634cd98c4651c0573a8c13cf05a8
oryngalikarimzhan <oryngalikarimzhan@github.com>
2021-12-13 18:22:24 +0600

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210 #239028]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,27 +4,20 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         String url = "https://www.sql.ru/forum/job-offers/";
-        List<Element> rowList = new ArrayList<>();
-        Document doc;
-        Elements row;
         for (int i = 1; i != 6; i++) {
-            url = url + i;
-            doc = Jsoup.connect(url).get();
-            row = doc.select(".postslisttopic");
-            row.stream().skip(4).forEach(rowList::add);
-            url = url.substring(0, url.length() - 1);
-        }
-        for (Element td : rowList) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
+            String path = String.format(url + "%s", i);
+            Document doc = Jsoup.connect(path).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(td.siblingElements().get(4).text());
+            }
         }
     }
-
 }
