14a999060efad77c35d8259b73ac21151decd96b
oryngalikarimzhan <oryngalikarimzhan@github.com>
2021-12-14 00:02:12 +0600

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/grabber/html/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/html/SqlRuParse.java
@@ -3,11 +3,12 @@ package ru.job4j.grabber.html;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.safety.Whitelist;
 import org.jsoup.select.Elements;
 import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.Post;
-import ru.job4j.grabber.utils.PostParser;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,8 +36,12 @@ public class SqlRuParse implements Parse {
             doc = Jsoup.connect(path).get();
             Elements row = doc.select(".postslisttopic");
             for (Element td : row) {
-                String postUrl = td.child(0).attr("href");
-                rsl.add(detail(postUrl));
+                Element href = td.child(0);
+                String title = href.text();
+                if (title.contains("java") && !title.contains("javascript")) {
+                    String postUrl = href.attr("href");
+                    rsl.add(detail(postUrl));
+                }
             }
         }
         return rsl;
@@ -44,6 +49,25 @@ public class SqlRuParse implements Parse {
 
     @Override
     public Post detail(String link) throws Exception {
-        return new PostParser().loadPost(link);
+        Document doc = Jsoup.connect(link).get();
+        doc.outputSettings(new Document.OutputSettings().prettyPrint(false));
+
+        String title = doc.select(".messageHeader").get(0).text();
+
+        Element desc = doc.select(".msgBody").get(1);
+        desc.select("br").after("\\n");
+        String str = desc
+                .html()
+                .replaceAll("\\\\n", "\n")
+                .trim();
+        String description = Jsoup.clean(str, "", Whitelist.none(), new Document.OutputSettings().prettyPrint(false));
+
+        String date = doc.select(".msgFooter")
+                .get(0)
+                .text()
+                .substring(0, 17);
+        LocalDateTime created = dateTimeParser.parse(date);
+
+        return new Post(link, title, description, created);
     }
 }
